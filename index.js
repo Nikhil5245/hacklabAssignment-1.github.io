87d01f71c9e3156417b730f12b6ac9f922bd9886
Nikhil <nikhil.rishav8541@gmail.com>
2023-04-12 20:29:04 +0530

    Traffic Light System

---
+++ index.js
@@ -0,0 +1,55 @@
+
+function waitfrome(ms){
+    return new Promise(resolve => {
+        setTimeout(() => {resolve('')},ms);
+    })
+}
+async function myFunction() {
+        
+    for(let i=1;i<=2;i++){
+        if(document.getElementById("Red").clicked == true)
+        {
+            document.getElementById("Green").style.backgroundColor = "Green";
+            await waitfrome(15000);
+            if(document.getElementById('Green').clicked == true){
+                document.getElementById("Yellow").style.backgroundColor = "Yellow";
+                await waitfrome(5000);
+                break;
+            }
+            document.getElementById("Yellow").style.backgroundColor = "Yellow";
+            await waitfrome(5000);
+            break;
+        }
+        document.getElementById("Red").style.backgroundColor = "Red";
+        await waitfrome(20000);
+        if(document.getElementById('Green').clicked == true){
+            document.getElementById("Yellow").style.backgroundColor = "Yellow";
+            await waitfrome(5000);
+        }
+        document.getElementById("Green").style.backgroundColor = "Green";
+        await waitfrome(15000);
+        if(document.getElementById('Yellow').clicked == true){
+            document.getElementById("Red").style.backgroundColor = "Red";
+        }
+        document.getElementById("Yellow").style.backgroundColor = "Yellow";
+        await waitfrome(5000);
+        if(document.getElementById("Red").clicked == true)
+        {
+            document.getElementById("Green").style.backgroundColor = "Green";
+            await waitfrome(15000);
+            if(document.getElementById('Green').clicked == true){
+                document.getElementById("Yellow").style.backgroundColor = "Yellow";
+                await waitfrome(5000);
+                break;
+            }
+            document.getElementById("Yellow").style.backgroundColor = "Yellow";
+            await waitfrome(5000);
+            break;
+        }
+        if(document.getElementById('Green').clicked == true){
+            document.getElementById("Yellow").style.backgroundColor = "Yellow";
+            await waitfrome(5000);
+        }
+        
+    }
+  }
